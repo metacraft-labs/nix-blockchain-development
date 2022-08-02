4974129169564da2d610e96a8d78d09971015b57
Dimo99 <dimodimov1999@gmail.com>
2022-08-02 20:42:26 +0300

    build(circom): Add package

--- overlay.nix
+++ overlay.nix
@@ -8,9 +8,12 @@ _finalNixpkgs: prevNixpkgs: let
   };
 
   cosmos-theta-testnet = prevNixpkgs.callPackage ./packages/cosmos-theta-testnet {};
+
+  circom = prevNixpkgs.callPackage ./packages/circom/default.nix {};
 in {
   metacraft-labs = rec {
     solana = solana-full-sdk;
     inherit cosmos-theta-testnet;
+    inherit circom;
   };
 }
