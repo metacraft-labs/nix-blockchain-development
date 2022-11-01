986edf2565ec7fae7fd7d2b0db202aa3cdcd3a22
monyarm <monyarm@gmail.com>
2022-11-01 15:09:56 +0200

    python module dependencies

--- packages/erdpy/default.nix
+++ packages/erdpy/default.nix
@@ -1,14 +1,42 @@
 {pkgs}:
 with pkgs;
-  python39Packages.buildPythonPackage rec {
+  python3Packages.buildPythonApplication rec {
     pname = "erdpy";
     version = "2.0.3";
-    src = python39Packages.fetchPypi {
-      inherit pname version;
-      sha256 = "08fdd5ef7c96480ad11c12d472de21acd32359996f69a5259299b540feba4560";
-  };
 
-    format = "setuptools";
+    format = "wheel";
+    dist = "py3";
+    python = "py3";
+
+
+    src = python3Packages.fetchPypi {
+      inherit pname version format dist python;
+    
+      sha256 = "215edfb6f9f8c8214cc42e67e8d5328859486007ac4e3438cb9aabd21de67414";
+    };
+
+
+  postPatch = ''
+    
+    chmod u+rwx -R ./dist
+    pushd dist
+    wheel unpack --dest unpacked ./*.whl
+    pushd unpacked/erdpy-${version}/erdpy-2.0.3.dist-info
+
+    # ledgercomm[hid] isn't a supported syntax by nix, so we split them into seperate requirements
+    sed -iE 's/Requires-Dist: ledgercomm.*/Requires-Dist: ledgercomm\nRequires-Dist: hid/' METADATA
+    
+    # # cryptography 36.0.2
+    # sed -iE 's/Requires-Dist: cryptography.*/Requires-Dist: cryptography/' METADATA
+    # cat METADATA
+    popd
+    wheel pack ./unpacked/erdpy-${version}
+    popd
+  '';
+
+    propagatedBuildInputs = with python3Packages; [
+      hid
+    ];
 
     meta = with lib; {
       homepage = "https://github.com/ElrondNetwork/elrond-sdk-erdpy";
