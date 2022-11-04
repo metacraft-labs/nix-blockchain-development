520f3cc1c75a55ea2d42ed6812ab16f787adc931
monyarm <monyarm@gmail.com>
2022-11-04 15:17:42 +0200

    alejandra formatting

--- packages/erdpy/default.nix
+++ packages/erdpy/default.nix
@@ -1,59 +1,57 @@
-{ pkgs }:
+{pkgs}:
 with pkgs;
-python3Packages.buildPythonApplication rec {
-  pname = "erdpy";
-  version = "2.0.3";
+  python3Packages.buildPythonApplication rec {
+    pname = "erdpy";
+    version = "2.0.3";
 
-  format = "wheel";
-  dist = "py3";
-  python = "py3";
+    format = "wheel";
+    dist = "py3";
+    python = "py3";
 
+    src = python3Packages.fetchPypi {
+      inherit pname version format dist python;
 
-  src = python3Packages.fetchPypi {
-    inherit pname version format dist python;
+      sha256 = "215edfb6f9f8c8214cc42e67e8d5328859486007ac4e3438cb9aabd21de67414";
+    };
 
-    sha256 = "215edfb6f9f8c8214cc42e67e8d5328859486007ac4e3438cb9aabd21de67414";
-  };
+    postPatch = ''
 
+      chmod u+rwx -R ./dist
+      pushd dist
+      wheel unpack --dest unpacked ./*.whl
+      pushd unpacked/erdpy-${version}/erdpy-2.0.3.dist-info
 
-  postPatch = ''
-    
-    chmod u+rwx -R ./dist
-    pushd dist
-    wheel unpack --dest unpacked ./*.whl
-    pushd unpacked/erdpy-${version}/erdpy-2.0.3.dist-info
+      # ledgercomm[hid] isn't a supported syntax by nix, so we split them into seperate requirements
+      sed -iE 's/Requires-Dist: ledgercomm.*/Requires-Dist: ledgercomm\nRequires-Dist: hid/' METADATA
 
-    # ledgercomm[hid] isn't a supported syntax by nix, so we split them into seperate requirements
-    sed -iE 's/Requires-Dist: ledgercomm.*/Requires-Dist: ledgercomm\nRequires-Dist: hid/' METADATA
-    
-    # # cryptography 36.0.2
-    # sed -iE 's/Requires-Dist: cryptography.*/Requires-Dist: cryptography/' METADATA
-    # cat METADATA
-    popd
-    wheel pack ./unpacked/erdpy-${version}
-    popd
-  '';
+      # # cryptography 36.0.2
+      # sed -iE 's/Requires-Dist: cryptography.*/Requires-Dist: cryptography/' METADATA
+      # cat METADATA
+      popd
+      wheel pack ./unpacked/erdpy-${version}
+      popd
+    '';
 
-  propagatedBuildInputs = with python3Packages; [
-    hid
-    metacraft-labs.cryptography36
-    metacraft-labs.ledgercomm
-    metacraft-labs.requests-cache
-  ];
+    propagatedBuildInputs = with python3Packages; [
+      hid
+      metacraft-labs.cryptography36
+      metacraft-labs.ledgercomm
+      metacraft-labs.requests-cache
+    ];
 
-  erdpy_script = writeScriptBin "erdpy" ''
-    #!/usr/bin/env python3
-    # -*- coding: utf-8 -*-
-    import re
-    import sys
-    from erdpy.cli import main
-    if __name__ == '__main__':
-        sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$\', \'\', sys.argv[0])
-        sys.exit(main())
-  '';
+    erdpy_script = writeScriptBin "erdpy" ''
+      #!/usr/bin/env python3
+      # -*- coding: utf-8 -*-
+      import re
+      import sys
+      from erdpy.cli import main
+      if __name__ == '__main__':
+          sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$\', \'\', sys.argv[0])
+          sys.exit(main())
+    '';
 
-  meta = with lib; {
-    homepage = "https://github.com/ElrondNetwork/elrond-sdk-erdpy";
-    platforms = with platforms; linux ++ darwin;
-  };
-}
+    meta = with lib; {
+      homepage = "https://github.com/ElrondNetwork/elrond-sdk-erdpy";
+      platforms = with platforms; linux ++ darwin;
+    };
+  }
--- packages/python-modules/ledgercomm/default.nix
+++ packages/python-modules/ledgercomm/default.nix
@@ -1,27 +1,27 @@
-{ pkgs }:
+{pkgs}:
 with pkgs;
-python3Packages.buildPythonPackage rec {
-  pname = "ledgercomm";
-  version = "1.1.0";
+  python3Packages.buildPythonPackage rec {
+    pname = "ledgercomm";
+    version = "1.1.0";
 
-  format = "wheel";
-  dist = "py3";
-  python = "py3";
+    format = "wheel";
+    dist = "py3";
+    python = "py3";
 
-  src = python3Packages.fetchPypi {
-    inherit pname version format dist python;
+    src = python3Packages.fetchPypi {
+      inherit pname version format dist python;
 
-    sha256 = "a85c16d3e2967ae6c0fa1bc9da2d99f766c166f32cd4d52a7e3537f2c849bf5f";
-  };
+      sha256 = "a85c16d3e2967ae6c0fa1bc9da2d99f766c166f32cd4d52a7e3537f2c849bf5f";
+    };
 
-  propagatedBuildInputs = with python3Packages; [
-    cython
-    hidapi
-    hid
-  ];
+    propagatedBuildInputs = with python3Packages; [
+      cython
+      hidapi
+      hid
+    ];
 
-  meta = with lib; {
-    homepage = "https://github.com/LedgerHQ/ledgercomm";
-    platforms = with platforms; linux ++ darwin;
-  };
-}
+    meta = with lib; {
+      homepage = "https://github.com/LedgerHQ/ledgercomm";
+      platforms = with platforms; linux ++ darwin;
+    };
+  }
