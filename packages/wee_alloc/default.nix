bda92add65e203ed5e883e0006d734473dba3a63
monyarm <monyarm@gmail.com>
2022-12-05 13:23:31 +0200

    wee_alloc Cargo.lock

--- packages/wee_alloc/default.nix
+++ packages/wee_alloc/default.nix
@@ -8,17 +8,18 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "wee_alloc";
-  version = "0.45";
+  version = "0.4.5";
 
   src = fetchgit {
     url = "https://github.com/rustwasm/wee_alloc";
     rev = "${version}";
-    sha256 = lib.fakeSha256;
+    # sha256 = lib.fakeSha256;
+    sha256 = "sha256-4MO7R7EVP3L16OLelmnyu41XtK69glnrk+97kfHaH7I=";
   };
 
   doCheck = false;
 
-  # cargoSha256 = lib.fakeSha256;
+  cargoSha256 = lib.fakeSha256;
   cargoLock.lockFile = "${src}/Cargo.lock";
 
 }
