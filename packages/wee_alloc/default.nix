a4b933494f2bca9949e51d0b74f3e00b8b5df294
monyarm <monyarm@gmail.com>
2022-12-06 13:08:18 +0200

    Cargo.toml patches

--- packages/wee_alloc/default.nix
+++ packages/wee_alloc/default.nix
@@ -5,10 +5,11 @@
   rustPlatform,
   fetchFromGitHub,
   fetchgit,
+  pkgs,
 }:
 rustPlatform.buildRustPackage rec {
   pname = "wee_alloc";
-  version = "0.4.5";
+  version = "0.4.4";
 
   src = fetchgit {
     url = "https://github.com/rustwasm/wee_alloc";
@@ -20,6 +21,25 @@ rustPlatform.buildRustPackage rec {
   doCheck = false;
 
   cargoSha256 = lib.fakeSha256;
-  cargoLock.lockFile = "${src}/Cargo.lock";
+  # cargoLock.lockFile = "${src}/Cargo.lock";
+  postPatch = ''
+    cp ${./Cargo.lock} Cargo.lock
+    cp ${./wee_alloc_Cargo.toml} wee_alloc/Cargo.toml
+    cp ${./trace-malloc-free_Cargo.toml} trace-malloc-free/Cargo.toml
+    cp ${./test_Cargo.toml} test/Cargo.toml
+    cp ${./example_Cargo.toml} example/Cargo.toml
+    # sed -i 's/{Alloc, AllocErr}/{Allocator, AllocError}/' wee_alloc/src/*.rs
+    # sed -i 's/AllocErr>/AllocError>/' wee_alloc/src/*.rs
+    # sed -i 's/ Alloc for/ Allocator for/' wee_alloc/src/*.rs
+  '';
 
+  nativeBuildInputs = with pkgs; [
+    rust-bin.nightly."2021-03-25".minimal #neeeded because wee_alloc is an older project, making use of experimental features
+  ];
+
+  cargoLock = let
+    fixupLockFile = path: (builtins.readFile path);
+  in {
+    lockFileContents = fixupLockFile ./Cargo.lock;
+  };
 }
