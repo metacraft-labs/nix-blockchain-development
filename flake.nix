5d3a82d32c859da4cfd9d76a6cea0cf354609884
Petar Kirov <petar.p.kirov@gmail.com>
2023-01-18 16:29:38 +0200

    build(flake): Use the same `flake-utils` and `nixpkgs` inputs for `rust-overlay`

--- flake.nix
+++ flake.nix
@@ -10,6 +10,8 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = github:numtide/flake-utils;
     rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    rust-overlay.inputs.flake-utils.follows = "flake-utils";
   };
 
   outputs = {
