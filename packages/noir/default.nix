812bf32d6cf258d2f7bb88fea17fa4b098a715f7
monyarm <monyarm@gmail.com>
2023-02-09 12:53:16 +0200

    Cargo.lock

--- packages/noir/default.nix
+++ packages/noir/default.nix
@@ -1,20 +1,35 @@
 {
   stdenv,
   lib,
-  darwin,
+  pkgs,
   rustPlatform,
-  fetchFromGitHub,
+  fetchgit,
 }:
 rustPlatform.buildRustPackage rec {
   pname = "noir";
-  version = "0.11";
+  version = "0.1.1";
 
-  src = fetchFromGitHub {
-    owner = "noir-lang";
-    repo = "noir";
+  src = fetchgit {
+    url = "https://github.com/noir-lang/noir";
     rev = "v${version}";
-    sha256 = lib.fakeSha256;
+    sha256 = "sha256-w5XcWL0H+EgRUZfD2tm8TGhSiyZPIWVNBmO7zxSnFbo=";
   };
 
-  cargoHash = lib.fakeSha256;
+  cargoSha256 = "sha256-T8dZRCX7Prz8AMD5rbJ1zQoN6XEslK33hjzG7T1LJog=";
+
+  buildNoDefaultFeatures = false;
+  buildFeatures = ["plonk_bn254_wasm"];
+  sourceRoot = "crates/nargo";
+
+  postPatch = ''
+    cp ${./Cargo.lock} Cargo.lock
+  '';
+
+  cargoLock = let
+    fixupLockFile = path: (builtins.readFile path);
+  in {
+    lockFileContents = fixupLockFile ./Cargo.lock;
+  };
+
+  nativeBuildInputs = with pkgs; [cmake llvmPackages.llvm llvmPackages.openmp pkgconfig];
 }
