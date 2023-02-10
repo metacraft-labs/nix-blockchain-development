9ee1c770588cc596e92fb5e7b8f51393791fd119
monyarm <monyarm@gmail.com>
2023-02-10 15:58:58 +0200

    Cargo.lock

--- packages/leo/default.nix
+++ packages/leo/default.nix
@@ -12,8 +12,18 @@ rustPlatform.buildRustPackage rec {
   src = fetchgit {
     url = "https://github.com/AleoHQ/leo";
     rev = "v${version}";
-    sha256 = fake.sha256;
+    sha256 = "sha256-is4i+8ChPEzpyEIiul+lxQgLyIB3pAubZjuIgC4W1VM=";
   };
 
-  cargoSha256 = fake.sha256;
+  cargoSha256 = "sha256-M5xWTN4is6nbXTEoQ97wiM0zhmJWFdUUif+Un0DlNS0=";
+
+  postPatch = ''
+    cp ${./Cargo.lock} Cargo.lock
+  '';
+
+  cargoLock = let
+    fixupLockFile = path: (builtins.readFile path);
+  in {
+    lockFileContents = fixupLockFile ./Cargo.lock;
+  };
 }
