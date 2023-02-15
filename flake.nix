4097f1fa6ee3c73718901faca30163f31516f48d
Petar Kirov <petar.p.kirov@gmail.com>
2023-02-15 23:44:12 +0200

    build(flake-inputs): Add oxalica/rust-overlay

--- flake.nix
+++ flake.nix
@@ -9,15 +9,24 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = github:numtide/flake-utils;
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    rust-overlay,
   }:
     {
-      overlays.default = import ./overlay.nix;
+      overlays.default =
+        nixpkgs.lib.fixedPoints.composeExtensions
+        rust-overlay.overlays.default
+        (import ./overlay.nix);
     }
     // (
       flake-utils.lib.eachDefaultSystem
