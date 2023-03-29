f15fab30321675b4a8941563627ce12fec946067
Petar Kirov <petar.p.kirov@gmail.com>
2023-03-29 14:04:54 +0300

    refactor(ffiasm-src): Improve derivation
    * List all deps explicitly and remove `with pkgs;`
    * Remove unused `gtest`, `gmp` and `nasm` deps
    * Use `dontNpmBuild` instead of setting our own `buildPhase`

--- packages/ffiasm/src.nix
+++ packages/ffiasm/src.nix
@@ -1,30 +1,27 @@
-{pkgs}:
-with pkgs;
-  buildNpmPackage rec {
-    pname = "ffiasm-src";
-    version = "0.1.4";
-    src = fetchFromGitHub {
-      owner = "iden3";
-      repo = "ffiasm";
-      rev = "v${version}";
-      hash = "sha256-nwDJi9HWCdhfUD3Os8MzngQq7SH6gx52vp77UwS2DLw=";
-    };
+{
+  lib,
+  buildNpmPackage,
+  fetchFromGitHub,
+}:
+buildNpmPackage rec {
+  pname = "ffiasm-src";
+  version = "0.1.4";
+  src = fetchFromGitHub {
+    owner = "iden3";
+    repo = "ffiasm";
+    rev = "v${version}";
+    hash = "sha256-nwDJi9HWCdhfUD3Os8MzngQq7SH6gx52vp77UwS2DLw=";
+  };
 
-    npmDepsHash = "sha256-xWXEcNDkIZhDjm5h6yweGkVjbo3mWKezg3wfTCkiOEE=";
+  npmDepsHash = "sha256-xWXEcNDkIZhDjm5h6yweGkVjbo3mWKezg3wfTCkiOEE=";
 
-    npmPackFlags = ["--ignore-scripts"];
+  npmPackFlags = ["--ignore-scripts"];
 
-    nativeBuildInputs = with pkgs; [gtest gmp nasm];
+  dontNpmBuild = true;
 
-    buildPhase = ''
-      runHook preBuild
-      echo "Nothing to build"
-      runHook postBuild
-    '';
-
-    meta = with lib; {
-      mainProgram = "buildzqfield";
-      homepage = "https://github.com/iden3/ffiasm";
-      platforms = with platforms; linux ++ darwin;
-    };
-  }
+  meta = {
+    mainProgram = "buildzqfield";
+    homepage = "https://github.com/iden3/ffiasm";
+    platforms = with lib.platforms; linux ++ darwin;
+  };
+}
