65fc15c8444fff2816ef52c9f666d40264025556
Petar Kirov <petar.p.kirov@gmail.com>
2023-03-29 13:34:01 +0300

    refactor(pistache): Improve derivation
    * List all deps explicitly and remove `with pkgs;`
    * Remove unused `gtest` and `doxygen` deps
    * Enable OpenSSL support and make it optional
    * Replace `with lib; with platforms;` with `lib.platforms.*`
    * Use `pname` attribute instead of name
    * Set platforms to Linux only, as per:
      https://github.com/pistacheio/pistache/issues/6#issuecomment-242398225

--- packages/pistache/default.nix
+++ packages/pistache/default.nix
@@ -1,26 +1,39 @@
-{pkgs}:
-with pkgs;
-  stdenv.mkDerivation rec {
-    name = "pistache-${version}";
-    version = "2023-02-25";
-    src = fetchFromGitHub {
-      owner = "pistacheio";
-      repo = "pistache";
-      rev = "ae073a0709ed1d6f0c28db90766c64b06f0366e6";
-      hash = "sha256-4mqiQRL3ucXudNRvjCExPUAlz8Q5BzEqJUMVK6f30ug=";
-    };
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  meson,
+  cmake,
+  ninja,
+  pkgconfig,
+  openssl,
+  rapidjson,
+  howard-hinnant-date,
+}:
+stdenv.mkDerivation rec {
+  pname = "pistache";
+  version = "2023-02-25";
+  src = fetchFromGitHub {
+    owner = "pistacheio";
+    repo = "pistache";
+    rev = "ae073a0709ed1d6f0c28db90766c64b06f0366e6";
+    hash = "sha256-4mqiQRL3ucXudNRvjCExPUAlz8Q5BzEqJUMVK6f30ug=";
+  };
 
-    nativeBuildInputs = with pkgs; [meson cmake ninja pkgconfig gtest];
+  nativeBuildInputs = [meson cmake ninja pkgconfig];
 
-    buildInputs = with pkgs; [
-      doxygen
-      openssl
-      rapidjson
-      howard-hinnant-date
-    ];
+  buildInputs = [
+    openssl
+    rapidjson
+    howard-hinnant-date
+  ];
 
-    meta = with lib; {
-      homepage = "https://github.com/pistacheio/pistache";
-      platforms = with platforms; linux ++ darwin;
-    };
-  }
+  mesonFlags =
+    lib.optional (openssl != null)
+    (lib.mesonOption "PISTACHE_USE_SSL" "true");
+
+  meta = {
+    homepage = "https://github.com/pistacheio/pistache";
+    platforms = lib.platforms.linux;
+  };
+}
