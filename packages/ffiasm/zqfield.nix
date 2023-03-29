2bfeab5f20690dbe95e1f1d5b70a12df0bba644d
Petar Kirov <petar.p.kirov@gmail.com>
2023-03-29 11:30:08 +0300

    refactor(ffiasm/zqfield): Simplify the package using `runCommand` and `lib.getExe`
    And also include the prime number in the package name.

--- packages/ffiasm/zqfield.nix
+++ packages/ffiasm/zqfield.nix
@@ -2,36 +2,21 @@
   lib,
   nasm,
   ffiasm-src,
-  nodejs,
-  stdenvNoCC,
+  runCommand,
+  hostPlatform,
 }: {
   primeNumber,
   name,
 }: let
   filename = lib.toLower name;
-
-  buildNasm =
-    if stdenvNoCC.isDarwin
-    then ''
-      nasm -fmacho64 --prefix _ ${filename}.asm
-    ''
-    else ''
-      nasm -felf64 ${filename}.asm
-    '';
+  nasmArgs =
+    if hostPlatform.isDarwin
+    then "-fmacho64 --prefix _"
+    else "-felf64";
 in
-  stdenvNoCC.mkDerivation {
-    name = "zqfield-${filename}";
-    unpackPhase = ":";
-    nativeBuildInputs = [nasm ffiasm-src];
-    buildPhase =
-      ''
-        ${ffiasm-src}/bin/buildzqfield -q '${primeNumber}' -n ${name}
-      ''
-      + buildNasm;
-    installPhase = ''
-      mkdir -p $out/lib
-      cp ${filename}.{asm,cpp,hpp,o} $out/lib/
-    '';
-
-    dontFixup = true;
-  }
+  runCommand "zqfield-${filename}-${primeNumber}" {} ''
+    ${lib.getExe ffiasm-src} -q ${primeNumber} -n ${name}
+    ${lib.getExe nasm} ${nasmArgs} ${filename}.asm
+    mkdir -p $out/lib
+    cp ${filename}.{asm,cpp,hpp,o} $out/lib/
+  ''
