7c691dd36ee4fc6d7cdaff4204c6584bc6f3f5c2
monyarm <monyarm@gmail.com>
2023-04-05 15:15:18 +0300

    base myhril package, py-ecc-410 and eth-utils-110

---
+++ overlay.nix
@@ -0,0 +1,164 @@
+_finalNixpkgs: prevNixpkgs: let
+  solana-rust-artifacts = prevNixpkgs.callPackage ./packages/solana-rust-artifacts {};
+
+  solana-bpf-tools = prevNixpkgs.callPackage ./packages/solana-bpf-tools {};
+
+  solana-full-sdk = prevNixpkgs.callPackage ./packages/solana-full-sdk {
+    inherit solana-rust-artifacts solana-bpf-tools;
+  };
+
+  cosmos-theta-testnet = prevNixpkgs.callPackage ./packages/cosmos-theta-testnet {};
+
+  circom = prevNixpkgs.callPackage ./packages/circom/default.nix {};
+  circ = prevNixpkgs.callPackage ./packages/circ/default.nix {};
+
+  wasmd = prevNixpkgs.callPackage ./packages/wasmd/default.nix {};
+
+  # erdpy depends on cattrs >= 22.2
+  cattrs22-2 = prevNixpkgs.python3Packages.cattrs.overrideAttrs (finalAttrs: previousAttrs: {
+    version = "22.2.0";
+
+    src = prevNixpkgs.fetchFromGitHub {
+      owner = "python-attrs";
+      repo = "cattrs";
+      rev = "v22.2.0";
+      hash = "sha256-Qnrq/mIA/t0mur6IAen4vTmMIhILWS6v5nuf+Via2hA=";
+    };
+
+    patches = [];
+  });
+
+  # https://discourse.nixos.org/t/inconsistent-vendoring-in-buildgomodule-when-overriding-source/9225/6
+  go-ethereum-capella = prevNixpkgs.go-ethereum.override rec {
+    buildGoModule = args:
+      prevNixpkgs.buildGoModule (args
+        // {
+          version = "1.11.1";
+          src = prevNixpkgs.fetchFromGitHub {
+            owner = "ethereum";
+            repo = "go-ethereum";
+            rev = "v1.11.1";
+            sha256 = "sha256-mYLxwJ0oiKfiz+NZ5bnlY0h2uq5wbeQKrwoCCw23Bg0=";
+          };
+          subPackages = builtins.filter (x: x != "cmd/puppeth") args.subPackages;
+          vendorSha256 = "sha256-6yLkeT5DrAPUohAmobssKkvxgXI8kACxiu17WYbw+n0=";
+        });
+  };
+
+  # copied from https://github.com/NixOS/nixpkgs/blob/8df7949791250b580220eb266e72e77211bedad9/pkgs/development/python-modules/cryptography/default.nix
+  cryptography36 = prevNixpkgs.callPackage ./packages/python-modules/cryptography36/default.nix {};
+
+  ledgercomm = prevNixpkgs.callPackage ./packages/python-modules/ledgercomm/default.nix {};
+  requests-cache = prevNixpkgs.callPackage ./packages/python-modules/requests-cache/default.nix {};
+
+  erdpy = prevNixpkgs.callPackage ./packages/erdpy/default.nix {};
+  elrond-go = prevNixpkgs.callPackage ./packages/elrond-go/default.nix {};
+  elrond-proxy-go = prevNixpkgs.callPackage ./packages/elrond-proxy-go/default.nix {};
+
+  go-opera = prevNixpkgs.callPackage ./packages/go-opera/default.nix {};
+
+  leap = prevNixpkgs.callPackage ./packages/leap/default.nix {};
+  eos-vm = prevNixpkgs.callPackage ./packages/eos-vm/default.nix {};
+  cdt = prevNixpkgs.callPackage ./packages/cdt/default.nix {};
+
+  nimbus = prevNixpkgs.callPackage ./packages/nimbus/default.nix {};
+
+  pistache = prevNixpkgs.callPackage ./packages/pistache/default.nix {};
+  ffiasm-src = prevNixpkgs.callPackage ./packages/ffiasm/src.nix {};
+  zqfield = prevNixpkgs.callPackage ./packages/ffiasm/zqfield.nix {
+    inherit ffiasm-src;
+  };
+  # Pairing Groups on BN-254, aka alt_bn128
+  # Source:
+  # https://zips.z.cash/protocol/protocol.pdf (section 5.4.9.1)
+  # See also:
+  # https://eips.ethereum.org/EIPS/eip-196
+  # https://eips.ethereum.org/EIPS/eip-197
+  # https://hackmd.io/@aztec-network/ByzgNxBfd
+  # https://hackmd.io/@jpw/bn254
+  zqfield-bn254 = prevNixpkgs.symlinkJoin {
+    name = "zqfield-bn254";
+    paths = [
+      (zqfield {
+        primeNumber = "21888242871839275222246405745257275088696311157297823662689037894645226208583";
+        name = "Fq";
+      })
+      (zqfield
+        {
+          primeNumber = "21888242871839275222246405745257275088548364400416034343698204186575808495617";
+          name = "Fr";
+        })
+    ];
+  };
+  ffiasm = prevNixpkgs.callPackage ./packages/ffiasm/default.nix {
+    inherit ffiasm-src zqfield-bn254;
+  };
+  circom_runtime = prevNixpkgs.callPackage ./packages/circom_runtime/default.nix {};
+  rapidsnark = prevNixpkgs.callPackage ./packages/rapidsnark/default.nix {
+    inherit ffiasm zqfield-bn254;
+  };
+  rapidsnark-server = prevNixpkgs.callPackage ./packages/rapidsnark-server/default.nix {
+    inherit ffiasm zqfield-bn254 rapidsnark pistache;
+  };
+
+  py-ecc-410 = prevNixpkgs.python3Packages.py-ecc.overrideAttrs (finalAttrs: previousAttrs: {
+    version = "4.1.0";
+
+    src = prevNixpkgs.fetchFromGitHub {
+      owner = "ethereum";
+      repo = "py_ecc";
+      rev = "v4.1.0";
+      hash = "sha256-qs4dvfdrl6o74FAst6XBAvzjJ7ZCA58s447aCTGIt2Y=";
+    };
+  });
+
+  eth-utils-110 = prevNixpkgs.python3Packages.eth-utils.overrideAttrs (finalAttrs: previousAttrs: {
+    version = "1.10.0";
+
+    src = prevNixpkgs.fetchFromGitHub {
+      owner = "ethereum";
+      repo = "eth-utils";
+      rev = "v1.10.0";
+      sha256 = "sha256-sq3H4HmUFUipqVYleZxWLG1gBsQEoNwcZAXiKckacek=";
+    };
+  });
+  mythril = prevNixpkgs.callPackage ./packages/python-modules/mythril/default.nix {inherit py-ecc-410 eth-utils-110;};
+in {
+  metacraft-labs = rec {
+    solana = solana-full-sdk;
+    inherit cosmos-theta-testnet;
+    inherit circom;
+
+    # Disabled until cvc4 compiles again
+    # inherit circ;
+
+    inherit wasmd;
+    inherit ledgercomm;
+    inherit cryptography36;
+    inherit requests-cache;
+    inherit erdpy;
+    inherit cattrs22-2;
+
+    # Disabled until elrond-go can build with Go >= 1.19
+    # inherit elrond-go;
+    # inherit elrond-proxy-go;
+    inherit go-opera;
+    inherit leap;
+    inherit eos-vm;
+    inherit cdt;
+
+    # Ethereum
+    inherit nimbus;
+    inherit go-ethereum-capella;
+
+    inherit pistache;
+    inherit zqfield-bn254;
+    inherit zqfield;
+    inherit ffiasm;
+    inherit circom_runtime;
+    inherit rapidsnark;
+    inherit rapidsnark-server;
+
+    inherit eth-utils-110 mythril;
+  };
+}
---
+++ packages/python-modules/mythril/default.nix
@@ -0,0 +1,51 @@
+{
+  lib,
+  stdenv,
+  python3Packages,
+  fetchFromGitHub,
+  py-ecc-410,
+  eth-utils-110,
+}:
+python3Packages.buildPythonPackage rec {
+  pname = "mythril";
+  version = "0.23.17";
+  format = "setuptools";
+
+  disabled = python3Packages.pythonOlder "3.6";
+
+  src = fetchFromGitHub {
+    owner = "ConsenSys";
+    repo = "mythril";
+    rev = "v${version}";
+    hash = "sha256-lzxAhaQfiGygxI98x1hWP8wrHVbf9DuZuEnjJcKMRCo=";
+  };
+
+  nativeBuildInputs = [
+  ];
+
+  propagatedBuildInputs = with python3Packages; [
+    py-ecc-410
+    eth-utils-110
+    setuptools
+  ];
+
+  # postFixup = lib.optionalString withSolc ''
+  #   wrapProgram $out/bin/myhtril \
+  #     --prefix PATH : "${lib.makeBinPath [solc]}"
+  # '';
+
+  # No Python tests
+  doCheck = false;
+
+  meta = with lib; {
+    description = "Security analysis tool for EVM bytecode.";
+    longDescription = ''
+      Mythril is a security analysis tool for EVM bytecode. It detects security vulnerabilities in smart contracts built
+      for Ethereum, Hedera, Quorum, Vechain, Roostock, Tron and other EVM-compatible blockchains. It uses
+      symbolic execution, SMT solving and taint analysis to detect a variety of security vulnerabilities. It's also used
+      (in combination with other tools and techniques) in the MythX security analysis platform.
+    '';
+    homepage = "https://github.com/ConsenSys/mythril";
+    license = licenses.mit;
+  };
+}
