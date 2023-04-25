775deeb25360fb7a33fa45eaff167be7331ba449
Petar Kirov <petar.p.kirov@gmail.com>
2023-04-26 01:45:35 +0300

    feat(packages): Integrate nix2container
    This resolves issue #52.

--- packages/default.nix
+++ packages/default.nix
@@ -2,12 +2,17 @@
   imports = [inputs.flake-parts.flakeModules.easyOverlay ./all-packages.nix];
   perSystem = {
     self',
+    inputs',
     ...
   }: {
     packages = self'.legacyPackages.metacraft-labs;
 
     overlayAttrs = {
-      inherit (self'.legacyPackages) metacraft-labs;
+      inherit (self'.legacyPackages) metacraft-labs nix2container;
+    };
+
+    legacyPackages = {
+      nix2container = inputs'.nix2container.packages.nix2container;
     };
   };
 }
