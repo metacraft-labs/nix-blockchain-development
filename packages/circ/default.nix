c0cda7cdf8d552cdf784d451048201771b607a5b
Petar Kirov <petar.p.kirov@gmail.com>
2023-04-27 14:15:51 +0300

    fix(circ): Fix issues and refactor the package
    * Use `fetchFromGitHub` and set `rev`
    * Set more appropriate version
    * Replace `with pkgs` with explicit reference to each package
    * Replace `cargoHash` with `cargoLock`
    * Fix build features by looking at the Cargo.toml file of the project
    * Update `meta`
    
    Also:
    * Re-enable the package in `shell.nix`

--- packages/circ/default.nix
+++ packages/circ/default.nix
@@ -1,39 +1,74 @@
-{pkgs}:
-with pkgs;
-  rustPlatform.buildRustPackage {
-    pname = "circ";
-    version = "0.0";
-    src = fetchgit {
-      url = "https://github.com/circify/circ";
-      sha256 = "sha256-Lu1GNTOQM3faEeMhmFX7F/GnfU35c0GgNMT6thoVZzs=";
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  pkg-config,
+  zlib,
+  gcc,
+  openssl,
+  cvc4,
+  cbc,
+  binutils,
+  gnum4,
+}:
+rustPlatform.buildRustPackage rec {
+  pname = "circ";
+  version = "unstable-2023-03-20";
+
+  src = fetchFromGitHub {
+    owner = "circify";
+    repo = "circ";
+    rev = "18990d079e988db842b83591528dc9739c3dbf9f";
+    hash = "sha256-/GVqlBacGdN6Cp0dxaHi4G13zeggdm75PFrftjStZTg=";
+  };
+
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    outputHashes = {
+      "bellman-0.13.1" = "sha256-e++6s64xJNagsBIBUfZ1RoXyN8taMdEfwcDPWKmp17Y=";
     };
+  };
 
-    cargoSha256 = "sha256-8iBvfpng0cQja8tiK6rk+a9DmoRy2pZtPXHfENgXZ8g=";
+  nativeBuildInputs = [
+    zlib
+    gcc
+    openssl
+    cvc4
+    cbc
+    binutils
+    gnum4
+  ];
 
-    nativeBuildInputs = with pkgs; [
-      zlib
-      gcc
-      openssl
-      cvc4
-      cbc
-      binutils
-      gnum4
-    ];
+  installPhase = ''
+    runHook preInstall
 
-    installPhase = ''
-      runHook preInstall
+    mkdir -p $out/bin
+    mv target/*/release/examples/circ $out/bin
 
-      mkdir -p $out/bin
-      mv target/*/release/examples/circ $out/bin
+    runHook postInstall
+  '';
 
-      runHook postInstall
-    '';
+  buildNoDefaultFeatures = true;
+  buildFeatures = [
+    "c"
+    "zok"
+    "datalog"
+    "smt"
+    "lp"
+    "aby"
+    "kahip"
+    "kahypar"
+    "r1cs"
+    "poly"
+    "spartan"
+    "bellman"
+  ];
 
-    buildNoDefaultFeatures = true;
-    buildFeatures = ["bls12381" "ff_dfl" "lang-c" "good_lp" "lp-solvers" "bellman" "rsmt2" "zokrates_parser" "zokrates_pest_ast"];
-
-    meta = with lib; {
-      homepage = "https://github.com/circify/circ";
-      platforms = with platforms; linux ++ darwin;
-    };
-  }
+  meta = with lib; {
+    description = "Cir)cuit (C)ompiler. Compiling high-level languages to circuits for SMT, zero-knowledge proofs, and more";
+    homepage = "https://github.com/circify/circ";
+    license = with licenses; [asl20 mit];
+    maintainers = with maintainers; [];
+    platforms = with platforms; linux ++ darwin;
+  };
+}
