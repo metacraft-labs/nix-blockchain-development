711468068c407d1438dd9189d05e98df20705135
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2023-05-02 18:06:57 +0300

    build(leap): Upgrade `leap` pkg to `v4.0.0`

--- packages/leap/default.nix
+++ packages/leap/default.nix
@@ -1,14 +1,29 @@
 {
   clangStdenv,
   nodejs,
-  fetchgit,
+  fetchFromGitHub,
   pkgs,
   lib,
   xz,
 }:
 clangStdenv.mkDerivation rec {
   name = "leap";
-  version = "3.2.0";
+  version = "4.0.0";
+
+  src = fetchFromGitHub {
+    owner = "AntelopeIO";
+    repo = "leap";
+    rev = "v${version}";
+    hash = "sha256-d9VjRfm+L7y8weADFnj9svm7go6HDyCKIsYsVuzUyZ4=";
+    fetchSubmodules = true;
+  };
+
+  prePatch = ''
+    find . -type f -name '*.py' -print0 | xargs -0 -I{} sed -i -E 's#/usr/bin/env python3?#${pkgs.python3}/bin/python3#' {}
+  '';
+
+  nativeBuildInputs = with pkgs; [pkgconfig cmake clang git python3];
+
   buildInputs = with pkgs; [
     llvm
     curl.dev
@@ -23,15 +38,4 @@ clangStdenv.mkDerivation rec {
         enabledStatic = true;
       })
   ];
-  nativeBuildInputs = with pkgs; [pkgconfig cmake clang git python3];
-
-  prePatch = ''
-    sed -i 's#/usr/bin/env python3#${pkgs.python3}/bin/python3#' **/*.py
-  '';
-
-  src = fetchgit {
-    url = "https://github.com/AntelopeIO/leap";
-    rev = "v${version}";
-    sha256 = "sha256-AQ8VsbP/14E/6gr1WNNCJJkpoiAe4liPfE9057fn5lc=";
-  };
 }
