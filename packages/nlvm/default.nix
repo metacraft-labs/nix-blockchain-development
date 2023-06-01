05d8b32f7b94842d0e77f2afc9f5fa61d34e2927
monyarm <monyarm@gmail.com>
2023-06-01 14:15:51 +0300

    nlvm WIP

--- packages/nlvm/default.nix
+++ packages/nlvm/default.nix
@@ -2,41 +2,61 @@
   pkgs,
   fetchgit,
   fetchurl,
+  symlinkJoin,
 }:
 pkgs.clangStdenv.mkDerivation rec {
   name = "nlvm-${version}";
-  version = "0";
+  version = "dev-2023-06-01";
 
   src = fetchgit {
     url = "https://github.com/arnetheduck/nlvm";
     fetchSubmodules = true;
     deepClone = true;
-    sha256 = "sha256-yTLySHTO+ZtgV1aacXfhQV0ZaLNf5d5PgCuzHyLJc9A";
-    rev = "92f43a3450e0b6f4999dc21e59b1bb6a1341ec85";
+    rev = "59db5053304d46aaf72a1c9e00b86353812eacbb";
+    hash = "sha256-rgq0Of7JrFDXIu5Hsz6bNkO3UY5uAeFz9AYEOfMkJ7Y=";
   };
 
-  LLVM_MAJ = "14";
-  LLVM_MIN = "0";
-  LLVM_PAT = "0";
-  LLVM_VER = "${LLVM_MAJ}.${LLVM_MIN}.${LLVM_PAT}";
-  SUFFIX = "x86_64-linux-gnu-ubuntu-18.04";
+  llvmEnv = symlinkJoin {
+    name = "nlvm-llvm-${version}";
+    paths = with pkgs.llvmPackages_16; [
+      clang-unwrapped
+      clang-unwrapped.lib
+      clang-unwrapped.dev
+      lld
+      lld.lib
+      lld.dev
+      llvm
+      llvm.lib
+      llvm.dev
+      libunwind
+      libunwind.dev
+      libclang
+      libclang.lib
+      libclang.dev
+      libcxx
+      libcxx.dev
+      libcxxabi
+      libcxxabi.dev
+      bintools
+    ];
+  };
 
-  clang_llvm = fetchurl {
-    url = "https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_VER}/clang+llvm-${LLVM_VER}-${SUFFIX}.tar.xz";
-    sha256 = "sha256-YVgiFdr6+3tXbqMMwTa+ksh3uh8cMd2703LW1lYi/vU=";
-  };
-  llvm = fetchurl {
-    url = "https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_VER}/llvm-${LLVM_VER}.src.tar.xz";
-    sha256 = "sha256-TfftULi3AXuQ3CIgL2tZ6QBqKalWgjjGryjfnASce5s=";
-  };
-  lld = fetchurl {
-    url = "https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_VER}/lld-${LLVM_VER}.src.tar.xz";
-    sha256 = "sha256-iPwPAoqowNkoeSCxAfKIsDx/q7WEB3MmxaC+SC65EVw=";
-  };
-  libunwind = fetchurl {
-    url = "https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_VER}/libunwind-${LLVM_VER}.src.tar.xz";
-    sha256 = "sha256-QJsxB5quh73GO5b/v9IrsUJRO+0Or9MncTkf8XSAJc4=";
-  };
+  # clang_llvm = fetchurl {
+  #   url = "https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_VER}/clang+llvm-${LLVM_VER}-${SUFFIX}.tar.xz";
+  #   sha256 = "sha256-YVgiFdr6+3tXbqMMwTa+ksh3uh8cMd2703LW1lYi/vU=";
+  # };
+  # llvm = fetchurl {
+  #   url = "https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_VER}/llvm-${LLVM_VER}.src.tar.xz";
+  #   sha256 = "sha256-TfftULi3AXuQ3CIgL2tZ6QBqKalWgjjGryjfnASce5s=";
+  # };
+  # lld = fetchurl {
+  #   url = "https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_VER}/lld-${LLVM_VER}.src.tar.xz";
+  #   sha256 = "sha256-iPwPAoqowNkoeSCxAfKIsDx/q7WEB3MmxaC+SC65EVw=";
+  # };
+  # libunwind = fetchurl {
+  #   url = "https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_VER}/libunwind-${LLVM_VER}.src.tar.xz";
+  #   sha256 = "sha256-QJsxB5quh73GO5b/v9IrsUJRO+0Or9MncTkf8XSAJc4=";
+  # };
 
   csources_v1 = fetchgit {
     url = "https://github.com/nim-lang/csources_v1";
@@ -48,6 +68,7 @@ pkgs.clangStdenv.mkDerivation rec {
 
   patches = [
     ./Makefile.diff
+    ./make-llvm.sh.diff
   ];
 
   meta = with pkgs.lib; {
@@ -57,14 +78,14 @@ pkgs.clangStdenv.mkDerivation rec {
 
   CC = "clang";
   CXX = "clang++";
-  C_INCLUDE_PATH = "${pkgs.gcc12}/lib/gcc/x86_64-unknown-linux-gnu/12.1.0/include:${pkgs.gcc12}/include/c++/12.1.0:${pkgs.gcc12}/lib/gcc/x86_64-unknown-linux-gnu/12.1.0/include-fixed:${pkgs.glibc.dev}/include";
+  C_INCLUDE_PATH = "${llvmEnv}/include:${llvmEnv}/include/c++/v1";
   CPLUS_INCLUDE_PATH = "${C_INCLUDE_PATH}";
 
-  CFLAGS = "-stdlib=libstdc++ -I${pkgs.gcc12}/lib/gcc/x86_64-unknown-linux-gnu/12.1.0/include -I${pkgs.gcc12}/include/c++/12.1.0 -I${pkgs.gcc12}/lib/gcc/x86_64-unknown-linux-gnu/12.1.0/include-fixed -I${pkgs.glibc.dev}/include";
+  CFLAGS = "-I${llvmEnv}/include -I${llvmEnv}/include/c++/v1";
   CPPFLAGS = "${CFLAGS}";
-  # LDFLAGS = "-lm -lc++ -lc -lunwind -L${pkgs.llvmPackages_14.lld.lib}/lib -L${pkgs.zlib}/lib -L${pkgs.llvmPackages_14.libcxx}/lib -L${pkgs.llvmPackages_14.libcxxabi}/lib -L${pkgs.glibc}/lib  -L${pkgs.llvmPackages_14.libunwind}/lib";
-  LDFLAGS = "-lm -lstdc++ -lc -lunwind -L../ext/llvm-14.0.0.src/sha/lib -L${pkgs.llvmPackages_14.lld.lib}/lib -Lext/libunwind-14.0.0.src/sha/lib -L${pkgs.zlib}/lib -L${pkgs.glibc}/lib -L${pkgs.llvmPackages_14.libunwind}/lib  -L${pkgs.gcc12}/lib";
-  LIBCLANG_PATH = "${pkgs.llvmPackages_14.libclang.lib}/lib";
+  LDFLAGS = "-lm -lc -lunwind";
+  LDLIBS = "-L${llvmEnv}/lib -L${pkgs.zstd.out}/lib";
+  LIBCLANG_PATH = "${llvmEnv}/lib";
 
   dontUseCmakeConfigure = true;
   dontUseNinjaBuild = true;
@@ -76,29 +97,24 @@ pkgs.clangStdenv.mkDerivation rec {
     export HOME=$TMPDIR
     mkdir -p ext
 
-    # sed "s#LLVMPATH=../ext#LLVMPATH='${pkgs.llvmPackages_14.llvm}'#" -i Makefile
-    # sed "s#LLVMRoot = \"../ext/llvm-14.0.0.src/\"#LLVMRoot = \"${pkgs.llvmPackages_14.llvm.dev}/\"#" -i llvm/llvm.nim
-    # sed "s#LLDRoot = \"../ext/lld-14.0.0.src/\"#LLDRoot = \"${pkgs.llvmPackages_14.lld.dev}/\"#" -i llvm/llvm.nim
-    # sed "s#LLVMOut = LLVMRoot & \"sha/\"#LLVMOut = LLVMRoot#" -i llvm/llvm.nim
-    # sed "s#LLVMOut = LLVMRoot & \"sta/\"#LLVMOut = LLVMRoot#" -i llvm/llvm.nim
-    # sed "s#sta/bin/llvm-config#/bin/llvm-config#" -i llvm/llvm.nim
-    # sed "s#\$ORIGIN/##" -i llvm/llvm.nim
-    # sed "s#-Wl,'##" -i llvm/llvm.nim
-    # sed "s#lib/'#lib/#" -i llvm/llvm.nim
-    # sed "s#-Wl,--as-needed#--as-needed#" -i llvm/llvm.nim
-
-
-
-    sed 's#LLVMRoot & \"sta/bin/llvm-config#\"${pkgs.llvmPackages_14.llvm.dev}/bin/llvm-config#' -i llvm/llvm.nim
-    sed 's#LLVMOut & \"bin/llvm-config#\"${pkgs.llvmPackages_14.llvm.dev}/bin/llvm-config#' -i llvm/llvm.nim
+    sed "s#LLVMPATH=../ext#LLVMPATH='${llvmEnv}'#" -i Makefile
+    sed "s#LLVMRoot = \"../ext/llvm-16.0.1.src/\"#LLVMRoot = \"${llvmEnv}/\"#" -i llvm/llvm.nim
+    sed "s#LLDRoot = \"../ext/lld-16.0.1.src/\"#LLDRoot = \"${llvmEnv}/\"#" -i llvm/llvm.nim
+    sed "s#LLVMOut = LLVMRoot & \"sha/\"#LLVMOut = LLVMRoot#" -i llvm/llvm.nim
+    sed "s#LLVMOut = LLVMRoot & \"sta/\"#LLVMOut = LLVMRoot#" -i llvm/llvm.nim
+    sed "s#sta/bin/llvm-config#/bin/llvm-config#" -i llvm/llvm.nim
+    sed "s#\$ORIGIN/##" -i llvm/llvm.nim
     sed "s#-Wl,'##" -i llvm/llvm.nim
+    sed "s#lib/'#lib/#" -i llvm/llvm.nim
     sed "s#-Wl,--as-needed#--as-needed#" -i llvm/llvm.nim
 
 
-    cp ${clang_llvm} ext/clang+llvm-${LLVM_VER}-${SUFFIX}.tar.xz
-    cp ${llvm} ext/llvm-${LLVM_VER}.src.tar.xz
-    cp ${lld} ext/lld-${LLVM_VER}.src.tar.xz
-    cp ${libunwind} ext/libunwind-${LLVM_VER}.src.tar.xz
+
+    sed 's#LLVMRoot & \"sta/bin/llvm-config#\"${llvmEnv}/bin/llvm-config#' -i llvm/llvm.nim
+    sed 's#LLVMOut & \"bin/llvm-config#\"${llvmEnv}/bin/llvm-config#' -i llvm/llvm.nim
+    sed "s#-Wl,'##" -i llvm/llvm.nim
+    sed "s#-Wl,--as-needed#--as-needed#" -i llvm/llvm.nim
+
 
     # mkdir -p Nim
     cp -r ${csources_v1} Nim/csources_v1
@@ -116,8 +132,8 @@ pkgs.clangStdenv.mkDerivation rec {
     clang.options = "${CFLAGS}"
     clang.cpp.options = "${CFLAGS}"
 
-    clang.options.linker = "${LDFLAGS}"
-    clang.cpp.options.linker = "${LDFLAGS}"
+    clang.options.linker = "${LDLIBS} ${LDFLAGS}"
+    clang.cpp.options.linker = "${LDLIBS} ${LDFLAGS}"
 
     gcc.cpp.exe %= "\$CXX"
     gcc.cpp.linkerexe %= "\$CXX"
@@ -129,22 +145,13 @@ pkgs.clangStdenv.mkDerivation rec {
 
   nativeBuildInputs = with pkgs; [
     zlib
-    llvmPackages_14.clang
-    llvmPackages_14.llvm
-    llvmPackages_14.lld
-    llvmPackages_14.libcxx
-    llvmPackages_14.libcxxabi
-    llvmPackages_14.libunwind
-    gcc12
     openssl
     wget
     pcre
     sqlite
     nim
     python3
-    glibc
-    # ninja
-    # cmake
+    llvmPackages_16.bintools
   ];
   buildInputs = with pkgs; [
     ninja
