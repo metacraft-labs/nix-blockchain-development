1b5c2939221adcc80fb234f80b839f239882f951
Petar Kirov <petar.p.kirov@gmail.com>
2023-07-10 15:25:39 +0300

    wip: build(solana-rust-artifacts): Rework the package based on `nixpkgs#solana-validator`
    TODO: check whether the BPF tools work.

--- packages/solana-rust-artifacts/default.nix
+++ packages/solana-rust-artifacts/default.nix
@@ -1,15 +1,4 @@
 {pkgs}:
-pkgs.solana-testnet.overrideAttrs (old: {
-  pname = "solana-rust-artifacts";
-  buildAndTestSubdir = null;
-  nativeBuildInputs = old.nativeBuildInputs ++ [pkgs.llvmPackages_13.clang];
-  buildInputs = old.buildInputs ++ [pkgs.openssl];
-  LIBCLANG_PATH = "${pkgs.llvmPackages_13.libclang.lib}/lib";
-  CARGO_FEATURE_VENDORED = "0";
-  OPENSSL_NO_VENDOR = "1";
-  postInstall = ''
-    mkdir -p $out/bin/sdk/
-    cp -r ./sdk/bpf $out/bin/sdk/
-  '';
-  patches = old.patches ++ [../cargo-build-bpf/patches/main.rs.diff];
+pkgs.solana-validator.overrideAttrs (old: {
+  # patches = old.patches ++ [../cargo-build-bpf/patches/main.rs.diff];
 })
