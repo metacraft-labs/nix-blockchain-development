2bc00e483b5e6679e28b4b695302ee496aa6560e
Petar Kirov <petar.p.kirov@gmail.com>
2023-07-10 12:44:35 +0300

    improve(scripts/commit_flake_update): Remove dependency on GitHub Actions CI

--- scripts/commit_flake_update.bash
+++ scripts/commit_flake_update.bash
@@ -8,27 +8,15 @@ if ! git config --get user.name >/dev/null 2>&1 || \
   [ "$(git config --get user.email)" = "" ]; then
   echo "git config user.{name,email} is not set - configuring"
   set -x
-  git config --local user.email "actions-bot"
-  git config --local user.name "actions-bot@users.noreply.github.com"
+  git config --local user.email "out@space.com"
+  git config --local user.name "beep boop"
 fi
 
 nix flake update --commit-lock-file
 
-cat >commit_msg <<EOF
+git commit --amend -F - <<EOF
 chore(flake.lock): Update all Flake inputs ($(date -I))
 
 $(git log -1 '--pretty=format:%b' | sed '1,2d')
 EOF
 
-git reset --soft 'HEAD~'
-
-if [[ -z "${GITHUB_ENV:-}" ]]; then
-  echo "Not running in CI - exiting"
-  exit 0
-fi
-
->> "$GITHUB_ENV" cat <<EOF
-COMMIT_MSG<<EOV
-$(cat ./commit_msg)
-EOV
-EOF
