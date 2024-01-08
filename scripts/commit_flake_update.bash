a8fad2ac81c24d420fc818d62a3116894ff8a8e6
Petar Kirov <petar.p.kirov@gmail.com>
2024-01-08 19:52:25 +0200

    improve(scripts): Allow updating specific inputs

--- scripts/commit_flake_update.bash
+++ scripts/commit_flake_update.bash
@@ -12,10 +12,28 @@ if ! git config --get user.name >/dev/null 2>&1 || \
   git config --local user.name "beep boop"
 fi
 
-nix flake update --commit-lock-file
+commit_prefix="build(flake.nix/inputs):"
+date="($(date -I))"
+
+if [ $# -eq 0 ]; then
+  command="nix flake update --commit-lock-file"
+  commit_title="$commit_prefix Update all Nix flake inputs $date"
+else
+  commit_title="$commit_prefix Update ["
+  command="nix flake lock --commit-lock-file"
+  for arg in "$@"; do
+    command+=" --update-input '$arg'"
+    commit_title+=" '$arg'"
+  done
+  commit_title+=" ] Nix flake inputs $date"
+fi
+
+(
+  eval "set -x; $command"
+)
 
 git commit --amend -F - <<EOF
-chore(flake.lock): Update all Flake inputs ($(date -I))
+$commit_title
 
 $(git log -1 '--pretty=format:%b' | sed '1,2d')
 EOF
