7337cd5f003b2dac547905d2a0ceb4eeaa0b7ab7
monyarm <monyarm@gmail.com>
2024-02-06 16:59:40 +0200

    feat(just/gh-actions): Matrix to markdown table in comment

--- shells/ci.nix
+++ shells/ci.nix
@@ -1,8 +1,32 @@
-{pkgs, ...}:
-pkgs.mkShellNoCC {
-  packages = with pkgs; [
-    just
-    jq
-    nix-eval-jobs
-  ];
-}
+{pkgs, ...}: let
+  commentablefmt =
+    pkgs.writers.writePython3Bin "commentablefmt" {
+      libraries = [pkgs.python3Packages.pytablewriter];
+    } ''
+      from pytablewriter import MarkdownTableWriter
+      import sys
+      import json
+
+
+      def main():
+          jsondata = json.load(sys.stdin)
+          writer = MarkdownTableWriter(
+            table_name="Matrix",
+            headers=["package", "x86_64-linux", "x86_64-darwin", "aarch64-darwin"],
+            value_matrix=jsondata
+          )
+          writer.write_table()
+
+
+      if __name__ == "__main__":
+          main()
+    '';
+in
+  pkgs.mkShellNoCC {
+    packages = with pkgs; [
+      just
+      jq
+      nix-eval-jobs
+      commentablefmt
+    ];
+  }
