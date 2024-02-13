f9909b141a6e4e24569ba88fc5a66d6c93400ad1
Petar Kirov <petar.p.kirov@gmail.com>
2024-02-13 02:00:25 +0200

    refactor(just): Extract targets as standalone scripts

--- shells/ci.nix
+++ shells/ci.nix
@@ -1,32 +1,7 @@
-{pkgs, ...}: let
-  commentablefmt =
-    pkgs.writers.writePython3Bin "commentablefmt" {
-      libraries = [pkgs.python3Packages.pytablewriter];
-    } ''
-      from pytablewriter import MarkdownTableWriter
-      import sys
-      import json
-
-
-      def main():
-          jsondata = json.load(sys.stdin)
-          writer = MarkdownTableWriter(
-            table_name="Matrix",
-            headers=["package", "x86_64-linux", "x86_64-darwin", "aarch64-darwin"],
-            value_matrix=jsondata
-          )
-          writer.write_table()
-
-
-      if __name__ == "__main__":
-          main()
-    '';
-in
-  pkgs.mkShellNoCC {
-    packages = with pkgs; [
-      just
-      jq
-      nix-eval-jobs
-      commentablefmt
-    ];
-  }
+{pkgs, ...}:
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    jq
+    nix-eval-jobs
+  ];
+}
