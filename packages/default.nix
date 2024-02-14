6caffcfb7944c60ca7b59019a7238408d5b886de
monyarm <monyarm@gmail.com>
2024-02-14 14:15:44 +0200

    refactor(flake outputs/ci-matrix): switch from evaling packages.${system}.metacraft-labs to checks

--- packages/default.nix
+++ packages/default.nix
@@ -1,4 +1,8 @@
-{inputs, ...}: {
+{
+  inputs,
+  lib,
+  ...
+}: {
   imports = [inputs.flake-parts.flakeModules.easyOverlay ./all-packages.nix];
   perSystem = {
     self',
@@ -25,8 +29,28 @@
     craneLib-nightly = (inputs.crane.mkLib pkgs).overrideToolchain rust-nightly;
 
     cardano-node = builtins.getFlake "github:input-output-hk/cardano-node/f0b4ac897dcbefba9fa0d247b204a24543cf55f6";
-  in {
+
+    reexportedPackages = {
+      ethereum_nix =
+        {
+          # geth = inputs'.ethereum_nix.packages.geth; # TODO: re-enable when flake show/check passes
+        }
+        // lib.optionalAttrs (pkgs.hostPlatform.isx86 && pkgs.hostPlatform.isLinux) {
+          # nimbus = inputs'.ethereum_nix.packages.nimbus-eth2; # TODO: re-enable when flake show/check passes
+        };
+      noir = {
+        nargo = inputs'.noir.packages.nargo;
+        noirc_abi_wasm = inputs'.noir.packages.noirc_abi_wasm;
+        acvm_js = inputs'.noir.packages.acvm_js;
+      };
+    };
+  in rec {
     packages = self'.legacyPackages.metacraft-labs;
+
+    checks =
+      self'.legacyPackages.metacraft-labs
+      // reexportedPackages.ethereum_nix
+      // reexportedPackages.noir;
 
     overlayAttrs = {
       inherit (self'.legacyPackages) metacraft-labs nix2container noir;
