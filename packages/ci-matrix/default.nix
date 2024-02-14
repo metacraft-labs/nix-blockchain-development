2ff0f58cc2b0312877b9ccb9c9d77d57486b00e3
monyarm <monyarm@gmail.com>
2024-02-14 17:35:56 +0200

    fix(ci-matrix): do not use sh instead of bash

--- packages/ci-matrix/default.nix
+++ packages/ci-matrix/default.nix
@@ -8,7 +8,6 @@ with pkgs;
 
     buildPhase = ''
       cp $src/{ci-matrix,nix-eval-jobs,system-info}.sh .
-      sed -i 's|/usr/bin/env bash|${busybox}/bin/sh|' *.sh
       sed -i 's|jq|${jq}/bin/jq|' *.sh
       sed -i 's|nix-eval-jobs |${nix-eval-jobs}/bin/nix-eval-jobs |' *.sh
       sed -i 's|"$root_dir/scripts/|"'$out'/bin/|' *.sh
