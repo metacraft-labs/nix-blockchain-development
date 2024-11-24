12276602329bba76410cbbc0c6091ad92ceb8348
Petar Kirov <petar.p.kirov@gmail.com>
2024-11-24 19:17:24 +0200

    build(pkgs/avalanche-cli): Specify package dependencies as explicit args

--- packages/avalanche-cli/default.nix
+++ packages/avalanche-cli/default.nix
@@ -1,23 +1,26 @@
-{pkgs}:
-with pkgs;
-  buildGoModule rec {
-    pname = "avalanche-cli";
-    version = "1.4.2";
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
+buildGoModule rec {
+  pname = "avalanche-cli";
+  version = "1.4.2";
 
-    src = fetchFromGitHub {
-      rev = "v${version}";
-      sha256 = "sha256-KhUPQVOHHbRNhnEzHVPSB1JMgtbJKsm2NYMtIAK8kk4=";
-      owner = "ava-labs";
-      repo = "avalanche-cli";
-    };
+  src = fetchFromGitHub {
+    owner = "ava-labs";
+    repo = "avalanche-cli";
+    rev = "v${version}";
+    hash = "sha256-KhUPQVOHHbRNhnEzHVPSB1JMgtbJKsm2NYMtIAK8kk4=";
+  };
 
-    doCheck = false;
-    proxyVendor = true;
-    vendorHash = "sha256-vhytojvmCOakN9RubjKkFnfA8tzOsOb+hKuACeQGSk4=";
+  doCheck = false;
+  proxyVendor = true;
+  vendorHash = "sha256-vhytojvmCOakN9RubjKkFnfA8tzOsOb+hKuACeQGSk4=";
 
-    meta = with lib; {
-      description = "Avalanche CLI is a command line tool that gives developers access to everything Avalanche.";
-      homepage = "https://github.com/ava-labs/avalanche-cli";
-      license = licenses.lgpl3;
-    };
-  }
+  meta = with lib; {
+    description = "Avalanche CLI is a command line tool that gives developers access to everything Avalanche.";
+    homepage = "https://github.com/ava-labs/avalanche-cli";
+    license = licenses.lgpl3;
+  };
+}
