d61fa1a2427e3d65ca043158a0c9ae4932ac4003
Kamen Mladenov <kamen@syndamia.com>
2024-12-04 13:44:54 +0200

    feat(pkgs/jolt): Implement package for version unstable-2024-11-22

---
+++ packages/jolt/default.nix
@@ -0,0 +1,46 @@
+{ rust-bin,
+  craneLib-stable,
+  fetchFromGitHub,
+  fetchurl,
+  pkg-config,
+  openssl,
+}:
+let
+  commonArgs = rec {
+    pname = "jolt";
+    version = "unstable-2024-12-04";
+
+    nativeBuildInputs = [
+      pkg-config
+      openssl
+    ];
+
+    preBuild = ''
+      sed -i 's/%2F/\//g' $CARGO_HOME/config.toml
+    '';
+
+    src = fetchFromGitHub {
+      owner = "a16z";
+      repo = "jolt";
+      rev = "1c5fad8a0857f9599a668336d57537f2dd61e68b";
+      hash = "sha256-VIFUqX5iTUyn+H0RgqmbXhvqkVI/akE9Ar/A33GuwDs=";
+      fetchSubmodules = true;
+    };
+  };
+
+  craneLib = craneLib-stable.overrideToolchain (rust-bin.fromRustupToolchainFile
+    (fetchurl {
+      url =
+      "https://raw.githubusercontent.com/${commonArgs.src.owner}/${commonArgs.src.repo}/${commonArgs.src.rev}/rust-toolchain.toml";
+      hash = "sha256-Fyj+Bp/dt3epuTN9kXN+r7Z3gzXYCDrcVEPWTr1sQqk=";
+    }));
+
+
+  cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+in
+  craneLib.buildPackage (commonArgs
+    // rec {
+      inherit cargoArtifacts;
+
+      doCheck = false;
+    })
