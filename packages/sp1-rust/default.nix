3f786661d4133242f3dd2b5722afe755cd7b01ca
Kamen Mladenov <kamen@syndamia.com>
2024-12-13 12:30:25 +0200

    feat(pkgs/sp1-rust): Implement fetching prebuilt sp1 rust compiler v1.81.0

---
+++ packages/sp1-rust/default.nix
@@ -0,0 +1,31 @@
+{ stdenv,
+  fetchGitHubReleaseAsset,
+  autoPatchelfHook,
+  zlib,
+  ...
+}:
+stdenv.mkDerivation rec {
+  name = "sp1-rust";
+  version = "1.81.0";
+
+  nativeBuildInputs = [
+    autoPatchelfHook
+    stdenv.cc.cc.lib
+    zlib
+  ];
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out
+    cp -r ./* $out/
+    runHook postInstall
+  '';
+
+  src = fetchGitHubReleaseAsset {
+    owner = "succinctlabs";
+    repo = "rust";
+    tag  = "v${version}";
+    asset = "rust-toolchain-x86_64-unknown-linux-gnu.tar.gz";
+    hash = "sha256-aj55nDtUhD1A6y5jsZDw6B/2RaK5yjvIyZdn8LC65UY=";
+  };
+}
