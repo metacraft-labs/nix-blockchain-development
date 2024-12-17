10dea376e471850e07e4ce98f7271a8800aea41d
Kamen Mladenov <kamen@syndamia.com>
2024-12-17 15:14:56 +0200

    fix(pkgs/jolt): Make cargo command select the correct binary depending on variable

--- packages/jolt/default.nix
+++ packages/jolt/default.nix
@@ -5,6 +5,7 @@
   installSourceAndCargo,
   pkg-config,
   openssl,
+  jolt-guest-rust,
   ...
 }:
 let
@@ -47,6 +48,22 @@ in
       postPatch = ''
         sed -i 's|package =.*git = "https://github.com/a16z/jolt"|path = "'$out'"|' src/main.rs
         sed -i '44,46d' jolt-core/src/host/toolchain.rs
+      '';
+
+      # Different toolchain is used when guest has std features
+      # https://github.com/a16z/jolt/blob/fa45507aaddb1815bafd54332e4b14173a7f8699/jolt-core/src/host/mod.rs#L132-L134
+      postInstall = ''
+        rm $out/bin/rustc
+        cat <<EOF > $out/bin/rustc
+        #!/bin/sh
+        if [ -z "\''${RUSTUP_TOOLCHAIN+x}" ]
+        then
+            ${jolt-guest-rust}/rust/build/host/stage2/bin/rustc \$@
+        else
+            ${rust-toolchain}/bin/rustc \$@
+        fi
+        EOF
+        chmod +x $out/bin/rustc
       '';
 
       doCheck = false;
