ac447f4d4a4f60289e458a71d17469b0e9b079a6
Kamen Mladenov <kamen@syndamia.com>
2024-12-17 15:14:56 +0200

    fix(pkgs/jolt): Make cargo command select the correct binary depending on variable

--- packages/all-packages.nix
+++ packages/all-packages.nix
@@ -224,8 +224,8 @@
           inherit corepack-shims;
 
           zkwasm = callPackage ./zkwasm/default.nix args-zkVM;
-          jolt = callPackage ./jolt/default.nix args-zkVM;
           jolt-guest-rust = callPackage ./jolt-guest-rust/default.nix args-zkVM-rust;
+          jolt = callPackage ./jolt/default.nix (args-zkVM // { inherit jolt-guest-rust; });
           zkm = callPackage ./zkm/default.nix args-zkVM;
           nexus = callPackage ./nexus/default.nix args-zkVM;
           sp1-rust = callPackage ./sp1-rust/default.nix args-zkVM-rust;
