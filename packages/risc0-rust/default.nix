d488666990d5102f51bbdc77f7211c6b96e8815f
Kamen Mladenov <kamen@syndamia.com>
2024-12-17 10:37:41 +0200

    chore(risc0-rust): Move leading version number to src.tag

--- packages/risc0-rust/default.nix
+++ packages/risc0-rust/default.nix
@@ -6,7 +6,7 @@
 }:
 stdenv.mkDerivation rec {
   name = "risc0-rust";
-  version = "0.1.81.0";
+  version = "1.81.0";
 
   nativeBuildInputs = [
     autoPatchelfHook
@@ -24,7 +24,7 @@ stdenv.mkDerivation rec {
   src = fetchGitHubReleaseAsset {
     owner = "risc0";
     repo = "rust";
-    tag  = "r${version}";
+    tag  = "r0.${version}";
     asset = "rust-toolchain-x86_64-unknown-linux-gnu.tar.gz";
     hash = "sha256-CzeZKT5Ubjk9nZZ2I12ak5Vnv2kFQNuueyzAF+blprU=";
   };
