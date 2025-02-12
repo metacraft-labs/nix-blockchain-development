236e7af41dc6991d024d12cd9272090ec974977b
Kamen Mladenov <kamen@syndamia.com>
2025-02-12 17:45:29 +0200

    chore(pkgs/risc0): Update to latest version (2025-02-12)
    RISC0 have diverged from using the same Rust version for host and guest
    building. Also, they have transformed rzup into a library, which is used
    to find the rust executable path.

--- packages/risc0/default.nix
+++ packages/risc0/default.nix
@@ -1,8 +1,10 @@
 {
   risc0-rust,
+  rust-bin,
   craneLib-nightly,
   fetchurl,
   fetchFromGitHub,
+  fetchGitHubFile,
   installSourceAndCargo,
   autoPatchelfHook,
   pkg-config,
@@ -14,16 +16,16 @@ let
   # https://github.com/risc0/risc0/blob/main/risc0/circuit/recursion/build.rs
   recursion-zkr =
     let
-      hash' = "ffc503386276f809137161f18d2f3ddcba3bb4b2d8b5d893b2c5d94b35afaf47";
+      hash' = "a1a9a1938e3143aecd995b8f20a93f3e1efb31d8b276dfa59acb9401bd2b36be";
     in
     fetchurl rec {
       url = "https://risc0-artifacts.s3.us-west-2.amazonaws.com/zkr/${hash'}.zip";
-      hash = "sha256-/8UDOGJ2+AkTcWHxjS893Lo7tLLYtdiTssXZSzWvr0c=";
+      hash = "sha256-oamhk44xQ67NmVuPIKk/Ph77Mdiydt+lmsuUAb0rNr4=";
     };
 
   commonArgs = rec {
     pname = "risc0";
-    version = "unstable-2024-12-21";
+    version = "unstable-2025-02-12";
 
     nativeBuildInputs = [
       autoPatchelfHook
@@ -34,12 +36,16 @@ let
     src = fetchFromGitHub {
       owner = "risc0";
       repo = "risc0";
-      rev = "3e042891fbad365fb1db7b17bd4afbb5e6fea99e";
-      hash = "sha256-wX8d44eGq1VxWMAduqPs8IMRMhcKcLuKKFhnRSQZeBc=";
+      rev = "f84f52be2b948ad121112329ab08e1722f839ebf";
+      hash = "sha256-8L35VByENIEC/su3lVTDo4Gnec7b6l68S86FLkHS2uU=";
     };
   };
 
-  rust-toolchain = risc0-rust;
+  rust-toolchain = rust-bin.fromRustupToolchainFile (fetchGitHubFile {
+    inherit (commonArgs.src) owner repo rev;
+    file = "rust-toolchain.toml";
+    hash = "sha256-n7Jr8rkovVQ98/KNvSg9EG9JZmKWD7DTaXTbpDJKA0Q=";
+  });
   craneLib = craneLib-nightly.overrideToolchain rust-toolchain;
   cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 in
@@ -52,8 +58,7 @@ craneLib.buildPackage (
     postPatch = ''
       # Replace usages of `Command::new("rustup")` with the correct value
       # which should be used
-      sed -i '82,89d;90ilet path_raw = Some("${rust-toolchain}");' risc0/cargo-risczero/src/toolchain.rs
-      sed -i '410,417d;663,681d;418ilet rustc = "${rust-toolchain}/bin/rustc";' risc0/build/src/lib.rs
+      sed -i '27d;28iPathBuf::from(r"${risc0-rust}")' rzup/src/paths.rs
       # Fix starter template
       sed -i 's|{{ risc0_build }}|path = "'$out'"|' risc0/cargo-risczero/templates/rust-starter/methods/Cargo-toml
       sed -i 's|{{ risc0_zkvm }}|path = "'$out'"|' risc0/cargo-risczero/templates/rust-starter/host/Cargo-toml
