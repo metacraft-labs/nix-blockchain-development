b722325b86b68e8038d2a28c77d45f8164843a66
Kamen Mladenov <kamen@syndamia.com>
2025-03-07 17:13:01 +0200

    chore(pkgs/zkvms): Update all to latest versio (2025-03-06)

--- packages/sp1/default.nix
+++ packages/sp1/default.nix
@@ -11,7 +11,7 @@
 let
   commonArgs = rec {
     pname = "sp1";
-    version = "unstable-2025-03-04";
+    version = "unstable-2025-03-06";
 
     nativeBuildInputs = [
       pkg-config
@@ -21,8 +21,8 @@ let
     src = fetchFromGitHub {
       owner = "succinctlabs";
       repo = "sp1";
-      rev = "b6eb0482541fe8816230229574a4fb199c5c01a1";
-      hash = "sha256-XdBQztyPJC6E9jXSRfO/lBGTFssDIEsU6ey8YoKmOGE=";
+      rev = "9f202bf603b3cab5b7c9db0e8cf5524a3428fbee";
+      hash = "sha256-RpllsIlrGyYw6dInN0tTs7K1y4FiFmrxFSyt3/Xelkg=";
       fetchSubmodules = true;
     };
   };
