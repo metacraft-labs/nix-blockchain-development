25a45f9f5cfd53e114bbea9ae70114aa2c1b870d
Petar Kirov <petar.p.kirov@gmail.com>
2025-06-04 12:49:37 +0300

    fix(pkgs/cdt): Rewrite derivation with nix-init

--- packages/cdt/default.nix
+++ packages/cdt/default.nix
@@ -1,32 +1,15 @@
 {
-  clangStdenv,
-  nodejs,
-  fetchFromGitHub,
-  pkgs,
   lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+  git,
+  python3,
 }:
-clangStdenv.mkDerivation rec {
-  name = "cdt";
+
+stdenv.mkDerivation rec {
+  pname = "cdt";
   version = "4.1.0";
-  buildInputs = with pkgs; [
-    llvm
-    curl.dev
-    gmp.dev
-    openssl.dev
-    libusb1.dev
-    bzip2.dev
-    (boost.override {
-      enableShared = false;
-      enabledStatic = true;
-    })
-  ];
-  nativeBuildInputs = with pkgs; [
-    pkg-config
-    cmake
-    clang
-    git
-    python3
-  ];
 
   src = fetchFromGitHub {
     owner = "AntelopeIO";
@@ -34,5 +17,19 @@ clangStdenv.mkDerivation rec {
     rev = "v${version}";
     hash = "sha256-+s+W2MBc/G2SCuBOdxdq661h5Oz1IH3z3HosrOQAbYU=";
     fetchSubmodules = true;
+  };
+
+  nativeBuildInputs = [
+    cmake
+    git
+    python3
+  ];
+
+  meta = {
+    description = "Contract Development Toolkit (CDT) is a suite of tools to facilitate C/C++ development of contracts for Antelope blockchains";
+    homepage = "https://github.com/AntelopeIO/cdt";
+    license = lib.licenses.mit;
+    mainProgram = "cdt";
+    platforms = lib.platforms.all;
   };
 }
