6bcf8445576950ce7b23aa1448e003b36c0941f8
reo101 <pavel.atanasov2001@gmail.com>
2025-09-10 09:18:37 +0300

    feat(pkgs/risc0): Update `unstable-2025-03-12` -> `v3.0.3`

--- packages/risc0/default.nix
+++ packages/risc0/default.nix
@@ -1,4 +1,5 @@
 {
+  lib,
   risc0-rust,
   rustFromToolchainFile,
   craneLib,
@@ -16,16 +17,20 @@ let
   # https://github.com/risc0/risc0/blob/main/risc0/circuit/recursion/build.rs
   recursion-zkr =
     let
-      hash' = "1b80b77894fbd489262e327478d02e83262c4bf189b0873fda3f0c85cdbfc8d1";
+      hash = "744b999f0a35b3c86753311c7efb2a0054be21727095cf105af6ee7d3f4d8849";
     in
-    fetchurl rec {
-      url = "https://risc0-artifacts.s3.us-west-2.amazonaws.com/zkr/${hash'}.zip";
-      hash = "sha256-G4C3eJT71IkmLjJ0eNAugyYsS/GJsIc/2j8Mhc2/yNE=";
+    fetchurl {
+      url = "https://risc0-artifacts.s3.us-west-2.amazonaws.com/zkr/${hash}.zip";
+      hash = builtins.convertHash {
+        inherit hash;
+        toHashFormat = "sri";
+        hashAlgo = "sha256";
+      };
     };
 
   commonArgs = rec {
     pname = "risc0";
-    version = "unstable-2025-03-12";
+    version = "3.0.3";
 
     nativeBuildInputs = [
       autoPatchelfHook
@@ -37,14 +42,14 @@ let
     src = fetchFromGitHub {
       owner = "risc0";
       repo = "risc0";
-      rev = "2db67acadc4e1283f08993b5dcfcfc7afba6bbbd";
-      hash = "sha256-eMFoz821x2NjibbTPF/i6rqRbqZ4g6njVDHc/udIDnA=";
+      rev = "v${version}";
+      hash = "sha256-39vVvvGcWbQOBm8G08GvjpSklMCjcGNq2+UabfU1+gs=";
     };
   };
 
   rust-toolchain = rustFromToolchainFile {
     dir = commonArgs.src;
-    sha256 = "sha256-s1RPtyvDGJaX/BisLT+ifVfuhDT1nZkZ1NcK8sbwELM=";
+    sha256 = "sha256-+9FmLhAOezBZCOziO0Qct1NOrfpjNsXxc/8I0c7BdKE=";
   };
   crane = craneLib.overrideToolchain rust-toolchain;
   cargoArtifacts = crane.buildDepsOnly commonArgs;
@@ -52,20 +57,34 @@ in
 crane.buildPackage (
   commonArgs
   // (installSourceAndCargo rust-toolchain)
-  // rec {
+  // {
     inherit cargoArtifacts;
 
-    postPatch = ''
-      # Replace usages of `Command::new("rustup")` with the correct value
-      # which should be used
-      sed -i '27d;28iPathBuf::from(r"${risc0-rust}")' rzup/src/paths.rs
+    # NOTE: leave ability to change comments without modifying the bash code, avoiding unneeded rebuilds
+    postPatch = lib.concatStringsSep "\n" [
+      # Replace references to the normal installations of `rustup` and `risc0`
+      # As per <https://github.com/risc0/risc0/blob/14b5d588dd01cf4f7ba804d8bb0a61264e6ae2c6/rzup/src/env.rs#L213>,
+      # used in places like <https://github.com/risc0/risc0/blob/14b5d588dd01cf4f7ba804d8bb0a61264e6ae2c6/rzup/src/components.rs#L139>
+      # bash
+      ''
+        RISC0_PATHBUF='PathBuf::from(r#"'"${risc0-rust}"'"#)'
+        sed -i \
+            -e '215s|home_dir.join(".risc0")|'"$RISC0_PATHBUF"'|' \
+            -e '219s|home_dir.join(".rustup")|'"$RISC0_PATHBUF"'|' \
+            ./rzup/src/env.rs
+      ''
+
       # Fix starter template
-      sed -i 's|{{ risc0_build }}|path = "'$out'"|' risc0/cargo-risczero/templates/rust-starter/methods/Cargo-toml
-      sed -i 's|{{ risc0_zkvm }}|path = "'$out'"|' risc0/cargo-risczero/templates/rust-starter/host/Cargo-toml
-    '';
+      # bash
+      ''
+        sed -i 's|{{ risc0_build }}|path = "'$out'"|' ./risc0/cargo-risczero/templates/rust-starter/methods/Cargo-toml
+        sed -i 's|{{ risc0_zkvm }}|path = "'$out'"|' ./risc0/cargo-risczero/templates/rust-starter/host/Cargo-toml
+      ''
+    ];
 
     preBuild = ''
-      export RECURSION_SRC_PATH="${recursion-zkr}" RUSTFLAGS="$RUSTFLAGS -A dead_code"
+      export RECURSION_SRC_PATH="${recursion-zkr}"
+      export RUSTFLAGS="$RUSTFLAGS -A dead_code"
     '';
 
     cargoBuildCommand = "cargo build --release -p risc0-zkvm -p risc0-build -p cargo-risczero --features unstable";
