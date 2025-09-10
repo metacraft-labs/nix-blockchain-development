9fe1a90c49e5beda33c4469ee89573a96de76026
reo101 <pavel.atanasov2001@gmail.com>
2025-09-10 09:18:37 +0300

    feat(pkgs/risc0-rust): Update `1.81.0` -> `1.88.0`

--- packages/risc0-rust/default.nix
+++ packages/risc0-rust/default.nix
@@ -7,7 +7,15 @@
 }:
 stdenv.mkDerivation rec {
   name = "risc0-rust";
-  version = "1.81.0";
+  version = "1.88.0";
+
+  src = fetchGitHubReleaseAsset {
+    owner = "risc0";
+    repo = "rust";
+    tag = "r0.${version}";
+    asset = "rust-toolchain-x86_64-unknown-linux-gnu.tar.gz";
+    hash = "sha256-hyqx546hXs2bN5GYbPJfSr/paKopGDK6EmJZ1Fq7Y60=";
+  };
 
   nativeBuildInputs = [
     autoPatchelfHook
@@ -32,12 +40,4 @@ stdenv.mkDerivation rec {
 
     runHook postInstall
   '';
-
-  src = fetchGitHubReleaseAsset {
-    owner = "risc0";
-    repo = "rust";
-    tag = "r0.${version}";
-    asset = "rust-toolchain-x86_64-unknown-linux-gnu.tar.gz";
-    hash = "sha256-CzeZKT5Ubjk9nZZ2I12ak5Vnv2kFQNuueyzAF+blprU=";
-  };
 }
